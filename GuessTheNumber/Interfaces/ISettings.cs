25b232ffe7d9beeb6c87961241c263b9d13d7da9
Marat <milya010790@mail.ru>
2025-03-04 15:43:40 +0500

    Для настроек

---
+++ GuessTheNumber/Interfaces/ISettings.cs
@@ -0,0 +1,14 @@
+﻿using GuessTheNumber.Models;
+
+namespace GuessTheNumber.Interfaces
+{
+    /// <summary>Настройки игры </summary>
+    public interface ISettings
+    {
+        /// <summary>Максимальное количество попыток</summary>
+        int MaxRetryCount { get; set; }
+
+        /// <summary><inheritdoc cref="RangeNumbers"/></summary>
+        RangeNumbers Range { get; set; }
+    }
+}
---
+++ GuessTheNumber/Models/RangeNumbers.cs
@@ -0,0 +1,21 @@
+﻿namespace GuessTheNumber.Models
+{
+    /// <summary>Задание диапазона чисел</summary>
+    public class RangeNumbers
+    {
+        /// <summary>Начальное число</summary>
+        public int Start { get; private set; }
+
+        /// <summary>Конечное число</summary>
+        public int End { get; private set; }
+
+        /// <summary><inheritdoc cref="RangeNumbers"/></summary>
+        /// <param name="start"><inheritdoc cref="Start" path="/summary/node()"/></param>
+        /// <param name="end"><inheritdoc cref="End" path="/summary/node()"/></param>
+        public RangeNumbers(int start, int end)
+        {
+            Start = start;
+            End = end;
+        }
+    }
+}
